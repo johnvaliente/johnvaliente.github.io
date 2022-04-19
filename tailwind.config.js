f1e0ac6ea90416b32e61092214fabbf9e39094b5
johnvaliente <jvali735@mtroyal.ca>
2022-04-19 11:41:04 -0600

    added new colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: [],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    colors: {
+      // Build your palette here
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.sky,
+      yellow: colors.amber,
+      coolGray: colors.coolGray,
+    }
+  }
 }
